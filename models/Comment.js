eb02266261ef3279029f1e154a3278e94582dec3
ARJUN <arjunsharman06@gmail.com>
2022-08-31 14:05:20 -0400

    added the reply schema

--- models/Comment.js
+++ models/Comment.js
@@ -1,16 +1,65 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const CommentSchema = new Schema({
-  writtenBy: {
-    type: String,
+// Replying schema : creating the sub-document
+const ReplySchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment _id
+    // Need to import Types at the top
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    replyBody: {
+      type: String,
+    },
+    writtenBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
   },
-  commentBody: {
-    type: String,
+  // Telling schema that it can use virtuals.
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const CommentSchema = new Schema(
+  {
+    writtenBy: {
+      type: String,
+    },
+    commentBody: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    //  association of  replies with comments
+    // Replies is directly neseted in comments.
+    // it not being ref like the pizza - comments
+    replies: [ReplySchema],
   },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
+  // Telling schema that it can use virtuals.
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual
+CommentSchema.virtual('replyCount').get(function () {
+  return this.replies.length;
 });
 
 // create the Comment model using the CommentSchema
