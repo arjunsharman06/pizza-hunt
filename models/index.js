5f425a1a981f9f268f95931558ba36359c472693
ARJUN <arjunsharman06@gmail.com>
2022-08-31 15:25:26 -0400

    changed the file name

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 // File that will package up all of the models.
-const Pizza = require('./Pizza');
+const Pizza = require('./pizza');
 const Comment = require('./Comment');
 
 module.exports = { Pizza, Comment };
